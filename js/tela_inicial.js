1ff34681b759df098c84eede2a747d9e703a31a8
felipebns <131299435+felipebns@users.noreply.github.com>
2023-12-05 21:25:28 -0300

    teste

--- js/tela_inicial.js
+++ js/tela_inicial.js
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //Verificando se foi completo uma ação
     fechou = localStorage.getItem('fim')
     acao = localStorage.getItem('ação')
-    if (fechou = true && acao != null){
+    if (fechou = true){
         fechou = false
         ac = document.querySelector('#add_acao')
         img = document.createElement('img')
