614abc4d5139a414d0c2bed193f78e6aa94f4a7b
felipebns <131299435+felipebns@users.noreply.github.com>
2023-12-05 21:13:40 -0300

    teste

--- js/fechando.js
+++ js/fechando.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let fechou = false
     botao = document.querySelector('#fechar')
     botao.addEventListener('click', function(event){
-        fechou = true
+        let fechou = true
         localStorage.setItem('fim', fechou)
     })
 })
